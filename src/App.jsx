d05a97dde0003275264cc00becf57b4915fad78b
SungilBang12 <butcherbirth@naver.com>
2025-11-13 15:00:53 +0900

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+          <h1>가연아 사랑해~</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
